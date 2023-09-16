381502288db642ef31bd9923e30f1decc5023bc5
devlcgomes <dev.lucianogomes@gmail.com>
2023-09-16 10:47:12 -0300

    Configuration of search by cep page

--- src/app/(cep)/cep/page.tsx
+++ src/app/(cep)/cep/page.tsx
@@ -1,10 +1,20 @@
 "use client";
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
+
+interface CepData {
+  bairro: string;
+  localidade: string;
+  uf: string;
+  cep: string;
+  logradouro: string;
+}
 
 const SearchByStreet: React.FC = () => {
   const [nomeRua, setNomeRua] = useState("");
-  const [cep, setCep] = useState("");
+  const [data, setData] = useState<CepData | null>(null);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNomeRua(event.target.value);
@@ -12,31 +22,77 @@ const SearchByStreet: React.FC = () => {
 
   const buscarCep = async () => {
     try {
-      const nomeRua = "Avenida Paulista";
+      if (!nomeRua) {
+        throw new Error("Digite o CEP");
+      }
+
+      setIsLoading(true);
       const response = await axios.get(
         `https://viacep.com.br/ws/${nomeRua}/json/`
       );
 
-      const data = response.data;
-      setCep(data.cep || "CEP não encontrado");
+      const data: CepData = response.data;
+
+      if (!data.cep) {
+        throw new Error("CEP não encontrado");
+      }
+
+      setData(data);
     } catch (error) {
-      console.error("Erro ao buscar CEP:", error);
-      setCep("Erro ao buscar CEP");
+      setError(error.message ?? "Erro ao buscar CEP");
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await buscarCep();
+      }}
+      className="p-6"
+    >
       <h2>Buscar CEP por Nome de Rua</h2>
       <input
         type="text"
-        placeholder="Digite o nome da rua"
+        placeholder="Digite o CEP"
         value={nomeRua}
         onChange={handleInputChange}
+        className="my-2 border border-purple-500 rounded-md p-2"
       />
-      <button onClick={buscarCep}>Buscar CEP</button>
-      <p>CEP: {cep}</p>
-    </div>
+
+      <button
+        className="mx-4 inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-purple-500 hover:bg-purple-400 transition ease-in-out duration-150"
+        onClick={buscarCep}
+      >
+        {isLoading ? "Carregando..." : "Buscar CEP"}
+      </button>
+
+      {!!error && <p className="text-red-500">{error}</p>}
+
+      {!!data && (
+        <div>
+          <hr className="my-4" />
+
+          <p>
+            <strong>CEP:</strong> {data.cep}
+          </p>
+
+          <p>
+            <strong>Bairro:</strong> {data.bairro}
+          </p>
+
+          <p>
+            <strong>Localidade:</strong> {data.localidade}
+          </p>
+
+          <p>
+            <strong>Logradouro:</strong> {data.logradouro}
+          </p>
+        </div>
+      )}
+    </form>
   );
 };
 
